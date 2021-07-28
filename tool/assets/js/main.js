028a8965a9e0090801c12bc8036caafc1809a936
nithikorn.s <nithikorn.s@sabuytech.com>
2021-07-28 17:34:30 +0700

    Clear Folder

--- tool/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(e){
-	$('#model-result-btn-submit').click(function(e){
-		window.location = $(this).attr('data-url');
-	});
-});
-function addCommas(val){
-    val = (val.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
-    return val;
-}
--- tool/setup.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	define('BASE', $base);
-	define('BASE_FOL', $base.'tool/');  
-	define('BASE_CATALOG', $base.'catalog/');
-	define('THEME','theme');
-	define('BASE_ASSET', MURL.'assets/'.THEME.'/');
-?>
